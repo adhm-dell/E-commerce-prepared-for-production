ce3fad8d19d7b9fdbf390cccf6243afc250a0147
unknown <adhmdoma11@gmail.com>
2025-07-28 05:23:45 +0300

    making my order detail page dynamic

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -47,7 +47,7 @@ Route::middleware('auth')->group(function () {
     })->name('logout');
     Route::get('/checkout', CheckoutPage::class)->name('checkout');
     Route::get('/my-orders', MyOrdersPage::class)->name('my-orders');
-    Route::get('/my-orders/{order}', MyOrderDetailPage::class)->name('my-orders.show');
+    Route::get('/my-orders/{order_id}', MyOrderDetailPage::class)->name('my-orders.show');
     Route::get('/success', SuccessPage::class)->name('payment.success');
     Route::get('/failed', CancelPage::class)->name('payment.failed');
 });
